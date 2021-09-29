9b5270e19591d8cce97b27e4c283cf90353d8fee
dr.dimitru <ceo@veliov.com>
2021-09-30 02:04:01 +0300

    Results + Claim

--- imports/client/templates/results/results.js
+++ imports/client/templates/results/results.js
@@ -1,32 +1,74 @@
-import './results.css';
-import './results.html';
+import { app } from '/imports/client/app.js';
+import { FlowRouter } from 'meteor/ostrio:flow-router-extra';
 import { Template } from 'meteor/templating';
-import { dummyData } from '../game/game';
+import { ReactiveVar } from 'meteor/reactive-var';
+import confetti from 'canvas-confetti';
+
+import './results.html';
 
 const throwConfettis = () => {
-  import confetti from 'canvas-confetti';
-
   confetti({
     particleCount: 200,
     spread: 100,
   });
 };
 
-const quiz = dummyData['1'];
 
 // With real data we would pass the quiz info here, and then set as the template state.
 Template.results.onCreated(function () {
-  throwConfettis();
-});
-Template.results.helpers({
-  quiz,
-});
-Template.results.events({
-  'click #leaderboard-button': (event, instance) => {
-    import { FlowRouter } from 'meteor/ostrio:flow-router-extra';
+  this.isLoading = new ReactiveVar(true);
+  this.claimProcessing = new ReactiveVar(false);
+  this.quizId = parseInt(this.data.params.quizId);
 
-    // state.get('_id')
-    FlowRouter.go('leaderboardRoute', { quizId: quiz.quizId });
+  (async () => {
+    this.game = await app.contract.get_game({ quiz_id: this.quizId, account_id: app._account.accountId });
+    if (!this.game) {
+      FlowRouter.go('home');
+    }
+
+    this.quiz = await app.contract.get_quiz({ quiz_id: this.quizId });
+    if (!this.quiz) {
+      FlowRouter.go('home');
+    }
+
+    this.reward = false;
+
+    if (this.quiz?.distributed_rewards?.length) {
+      for (const reward of this.quiz.distributed_rewards) {
+        if (reward.winner_account_id === app._account.accountId) {
+          this.reward = reward;
+          this.reward.amountInt = app.formatNearAmount(this.reward.amount);
+          throwConfettis();
+          break;
+        }
+      }
+    }
+
+    this.isLoading.set(false);
+  })();
+});
+
+Template.results.helpers({
+  isLoading() {
+    return Template.instance().isLoading.get();
   },
-  'click #claim-button': (event, instance) => {},
+  claimProcessing() {
+    return Template.instance().claimProcessing.get();
+  },
+  quiz() {
+    return Template.instance().quiz;
+  },
+  reward() {
+    return Template.instance().reward;
+  }
+});
+
+Template.results.events({
+  async 'click [data-claim]' (e, template) {
+    e.preventDefault();
+    template.claimProcessing.set(true);
+    await app.contract.claim_reward({ quiz_id: template.quizId });
+    FlowRouter.go('home');
+    return false;
+  }
 });
