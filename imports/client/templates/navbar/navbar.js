40d580f5e659ae42f970254e8862ac1135dfb0d4
dr.dimitru <ceo@veliov.com>
2021-09-30 12:55:34 +0300

    Make user-profile button nicer
    - Initially we expected to get avatar from NEAR API; But no user-image data is available; going to use user's ID first letter

--- imports/client/templates/navbar/navbar.js
+++ imports/client/templates/navbar/navbar.js
@@ -1,2 +1,13 @@
-// import { Template } from 'meteor/templating';
+import { app } from '/imports/client/app.js';
+import { Template } from 'meteor/templating';
 import './navbar.html';
+
+Template.navbar.helpers({
+  usernameFirstLetter() {
+    const user = app.user.get();
+    if (user?.accountId) {
+      return user.accountId[0].toUpperCase();
+    }
+    return '⚙️';
+  }
+});
