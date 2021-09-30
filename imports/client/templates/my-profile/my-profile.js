63a715768a49944aa7eab038c5ccf88681a7e480
dr.dimitru <ceo@veliov.com>
2021-09-30 16:16:29 +0300

    Profile: codebase cleanup

--- imports/client/templates/my-profile/my-profile.js
+++ imports/client/templates/my-profile/my-profile.js
@@ -1,17 +1,12 @@
 import { app } from '/imports/client/app.js';
 import { FlowRouter } from 'meteor/ostrio:flow-router-extra';
 import { Template } from 'meteor/templating';
-// import { ReactiveVar } from 'meteor/reactive-var';
 import './my-profile.html';
 
 Template.myProfile.onCreated(function () {
   if (!app._account) {
     FlowRouter.go('home');
   }
-});
-
-Template.myProfile.helpers({
-
 });
 
 Template.myProfile.events({
